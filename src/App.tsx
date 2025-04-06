5344ae0749b58c906efd7ab6862b8dd716a5806a
Jedme Silva <157550741+jedmesilva@users.noreply.github.com>
2025-04-06 13:38:52 +0000

    RestartUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "@/lib/queryClient";
+import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
@@ -7,7 +7,7 @@ import HomePage from "@/pages/home-page";
 import AuthPage from "@/pages/auth-page";
 import LandingPage from "@/pages/landing-page";
 import LetterPage from "@/pages/letter-page";
-import { ProtectedRoute } from "@/lib/protected-route";
+import { ProtectedRoute } from "./lib/protected-route";
 import { SupabaseAuthProvider } from "./hooks/use-supabase-auth";
 
 function Router() {
